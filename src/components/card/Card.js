b6b931a1f52926117984fe02d3659f9267afc758
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-03-26 18:47:25 +0300

    second commit

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -6,7 +6,7 @@ const Card = () => {
   return (
     <div className="cards-area-container">
       <div className="bars"></div>
-      <h1 className="language-title">Languages</h1>
+      <h1 className="language-title">Languages Update</h1>
       <div className="cards-container">
         {languages.map((item, index) => {
           return <Item card={item} key={index} />;
